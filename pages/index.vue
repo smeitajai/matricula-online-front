c65e860c1609a04e51bcad3889b0f6c984cc8f1d
Darlan Voss <darlanvoss@gmail.com>
2023-07-14 18:27:39 -0300

    feat: Criar Component Core/Card

--- pages/index.vue
+++ pages/index.vue
@@ -2,37 +2,45 @@
   <v-container fluid class="login-page fill-height">
     <v-row justify="center">
       <v-col align-self="center" cols="12" md="8">
-        <v-card class="elevation-12" rounded="lg">
-          <v-toolbar color="primary">
-            <v-toolbar-title>Matrícula On-line</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <h2>
-              Seja bem-vindo(a) ao novo sistema de Matrículas On-line do
-              município de Itajaí/SC!
-            </h2>
+        <CoreCard rounded="lg" title="Matrícula On-line" toolbar>
+          <v-row>
+            <span class="text-h6 ma-5">
+              Seja bem-vindo(a) ao novo sistema de Matrículas On-line para o
+              ensino fundamental do município de Itajaí/SC!
+            </span>
+          </v-row>
 
-            <br />
-            <p>
+          <v-row>
+            <span class="mx-5">
               Com apenas alguns cliques, você pode realizar o processo de forma
               simples e segura.
-            </p>
-            <br />
+            </span>
+          </v-row>
 
-            <p class="font-weight-bold">
-              Matrícula Interna - 07/11/2023 até 10/11/2023
-            </p>
-            <p>Transferência Interna - 21/11/2023 até 24/11/2023</p>
-            <p>Matrículas Novas - 05/12/2023 até 08/12/2023</p>
-            <br />
+          <v-row class="my-8 pl-5">
+            <v-col cols="12" class="pa-0"> Datas Importantes: </v-col>
+            <v-col cols="12" class="pa-0">
+              <span class="font-weight-bold">
+                Matrícula Interna - 07/11/2023 até 10/11/2023
+              </span>
+            </v-col>
+            <v-col cols="12" class="pa-0">
+              <span>Transferência Interna - 21/11/2023 até 24/11/2023</span>
+            </v-col>
+            <v-col cols="12" class="pa-0">
+              <span>Matrículas Novas - 05/12/2023 até 08/12/2023</span>
+            </v-col>
+          </v-row>
+
+          <v-row>
             <a
               href="https://drive.google.com/file/d/1C6uFVd71HnQxWkg8czDpsYFu5Mk5poLM/view"
               target="_blank"
-              class="text-h5"
+              class="text-h5 mx-5 pb-5"
               >Clique aqui para acessar o edital de matrícula {{ anoEdital }}</a
             >
-          </v-card-text>
-        </v-card>
+          </v-row>
+        </CoreCard>
       </v-col>
       <v-col cols="12" class="mt-5">
         <v-row justify="center">
