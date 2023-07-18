ad3c6db3be77b41762d738599a69d09e644e30a9
Darlan Voss <darlanvoss@gmail.com>
2023-07-18 13:55:09 -0300

    refactor: Melhorado lógica de exibição do Header no Layout default

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,15 @@
   <div>
     <v-app dark>
       <v-main>
-        <Header v-if="$route.path != '/'" />
+        <Header v-if="isNotHome" />
         <slot />
         <Footer />
       </v-main>
     </v-app>
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+const isNotHome = computed(() => (route.path != "/" ? true : false));
+</script>
