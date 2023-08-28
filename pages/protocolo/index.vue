8b9766525b456750730bfa6461ef5e65f5bb724f
Darlan Voss <darlanvoss@gmail.com>
2023-08-28 16:37:31 -0300

    feat(Protocolo): Cria view de Protocolo.

--- pages/protocolo/index.vue
+++ pages/protocolo/index.vue
@@ -1,33 +1,47 @@
 <template>
   <ProtocoloInfo />
-  <v-row justify="end" class="ma-5">
-    <CoreButton
-      id="print-btn"
-      label="Imprimir"
-      prepend-icon="mdi-printer"
-      @click="onImprimir()"
-    />
+  <v-row id="protocol" justify="end" class="ma-5">
+    <div class="mx-2">
+      <CoreButton
+        label="Início"
+        prepend-icon="mdi-apps"
+        @click="onClickInicio()"
+      />
+    </div>
+    <div class="mx-2">
+      <CoreButton
+        label="Imprimir"
+        prepend-icon="mdi-printer"
+        @click="onClickImprimir()"
+      />
+    </div>
   </v-row>
 </template>
 
 <script setup>
-const onImprimir = () => {
+const onClickImprimir = () => {
   const appbar = document.querySelector("#app-bar");
-  const printBtn = document.querySelector("#print-btn");
+  const protocol = document.querySelector("#protocol");
   const footer = document.querySelector("#footer");
 
   window.addEventListener("beforeprint", () => {
     appbar.style.display = "none";
-    printBtn.style.display = "none";
+    protocol.style.display = "none";
     footer.style.display = "none";
   });
 
   window.addEventListener("afterprint", () => {
     appbar.style.display = "inline";
-    printBtn.style.display = "inline-grid";
+    protocol.style.display = "flex";
     footer.style.display = "inline";
   });
 
   window.print();
+};
+
+const onClickInicio = async () => {
+  await navigateTo({
+    path: "/",
+  });
 };
 </script>
