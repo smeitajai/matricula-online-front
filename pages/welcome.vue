977ae6c7916b2691d171d24d7359487b55f28187
Darlan Voss <darlanvoss@gmail.com>
2023-09-14 17:31:04 -0300

    feat(Welcome): Adiciona Welcome Page

---
+++ pages/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <v-row class="pa-5">
+      <v-col cols="12" class="text-h4">
+        Boas-vindas ao sistema de Matrículas On-line!
+      </v-col>
+      <v-col cols="12" class="text-overline">
+        Utilize o menu lateral 
+        <v-icon>mdi-menu</v-icon>
+        para acessar as opções.
+      </v-col>
+    </v-row>
+    
+  </v-container>
+</template>
