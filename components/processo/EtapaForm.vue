64f081766f79499e72f2d5248a1600ed402bf655
Darlan Voss <darlanvoss@gmail.com>
2023-10-03 16:28:41 -0300

    feat: Cria views e components para Etapas do Processo

---
+++ components/processo/EtapaForm.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-form ref="form">
+    <v-row class="ma-0">
+      <CoreInput
+        v-model="dadosEtapa.nome"
+        full-width
+        label="Nome*"
+        required
+        @input="dadosEtapa.nome = $event"
+      />
+      <CoreFormSubtitle label="Fase Inicial" class="pt-0" />
+      <CoreInput
+        v-model="dadosEtapa.faseInicialDataInicio"
+        label="Data Inicial*"
+        type="date"
+        required
+        @input="dadosEtapa.faseInicialDataInicio = $event"
+      />
+      <CoreInput
+        v-model="dadosEtapa.faseInicialDataFim"
+        label="Data Final*"
+        type="date"
+        required
+        @input="dadosEtapa.faseInicialDataFim = $event"
+      />
+
+      <CoreFormSubtitle label="Fase Final" class="pt-0" />
+      <CoreInput
+        v-model="dadosEtapa.faseFinalDataInicio"
+        label="Data Inicial*"
+        type="date"
+        required
+        @input="dadosEtapa.faseFinalDataInicio = $event"
+      />
+      <CoreInput
+        v-model="dadosEtapa.faseFinalDataFim"
+        label="Data Final*"
+        type="date"
+        required
+        @input="dadosEtapa.faseFinalDataFim = $event"
+      />
+    </v-row>
+  </v-form>
+</template>
+
+<script setup>
+import { format, utcToZonedTime } from "date-fns-tz";
+
+const form = ref(null);
+
+const emit = defineEmits(["input", "valid"]);
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: () => ({}),
+  },
+  validate: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+watch(
+  () => props.validate,
+  async () => {
+    const { valid } = await form.value.validate();
+    emit("valid", valid);
+  },
+);
+
+onMounted(() => {
+  if (props.modelValue.id) {
+    formatData(dadosEtapa.value);
+  }
+});
+
+const dadosEtapa = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(dadosForm) {
+    emit("input", dadosForm);
+  },
+});
+
+const formatData = (etapa) => {
+  // "-00:00" Ignora o timezone, mantendo a data correta
+  etapa.faseInicialDataInicio = format(
+    utcToZonedTime(etapa.faseInicialDataInicio, "-00:00"),
+    "yyyy-MM-dd",
+  );
+  etapa.faseInicialDataFim = format(
+    utcToZonedTime(etapa.faseInicialDataFim, "-00:00"),
+    "yyyy-MM-dd",
+  );
+  etapa.faseFinalDataInicio = format(
+    utcToZonedTime(etapa.faseFinalDataInicio, "-00:00"),
+    "yyyy-MM-dd",
+  );
+  etapa.faseFinalDataFim = format(
+    utcToZonedTime(etapa.faseFinalDataFim, "-00:00"),
+    "yyyy-MM-dd",
+  );
+};
+</script>
---
+++ pages/processo/etapas.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-row class="ma-5">
+    <PageTitle
+      v-if="processo"
+      :title="`Etapas do Processo | ${processo.nome}`"
+    />
+    <v-col cols="12">
+      <CoreList
+        :item-text="(e) => e.nome"
+        :item-text-sub="
+          (e) =>
+            `Início: ${formatarData(
+              e.faseInicialDataInicio,
+            )} | Fim: ${formatarData(e.faseFinalDataFim)}`
+        "
+        :items="etapasProcesso"
+        header="Etapas Cadastradas"
+        toolbar
+        @click="onClickItem($event)"
+      />
+    </v-col>
+  </v-row>
+
+  <ProcessoEtapaDialog
+    :dialog="dialog"
+    :etapa="etapaSelected"
+    :processo="processo"
+    @close="dialog = false"
+    @created="onCreate($event)"
+    @updated="onUpdate($event)"
+  />
+
+  <CoreFloatingButton
+    icon="mdi-plus"
+    tooltip="Adicionar Etapa"
+    @click="(etapaSelected = {}), (dialog = true)"
+  />
+
+  <CoreSnackbar
+    v-model="showMessage"
+    color="error"
+    :message="message"
+    @hide="showMessage = $event"
+  />
+</template>
+
+<script setup>
+import { format, utcToZonedTime } from "date-fns-tz";
+
+const route = useRoute();
+const { data: processo, error: errorProcesso } = await useFetch(
+  `/api/processos/${route.query.processo}`,
+);
+const { data: etapasProcesso, error: errorEtapas } = await useFetch(
+  "/api/processo-etapas",
+  {
+    query: {
+      // Esse filtro ainda não funciona
+      processo: route.query.processo,
+    },
+  },
+);
+
+// Filtro Paliativo para exibir apenas as etapas do processo selecionado
+etapasProcesso.value = etapasProcesso.value.filter(
+  (e) => e.processoId == processo.value.id,
+);
+
+const showMessage = ref(false);
+const message = ref("");
+const dialog = ref(false);
+const etapaSelected = ref({});
+
+onMounted(() => {
+  if (errorProcesso.value || (processo.value && processo.value.error)) {
+    errorProcesso.value
+      ? (message.value = errorProcesso.value.message)
+      : (message.value = processo.value.message);
+    return (showMessage.value = true);
+  }
+
+  if (
+    errorEtapas.value ||
+    (etapasProcesso.value && etapasProcesso.value.error)
+  ) {
+    errorEtapas.value
+      ? (message.value = errorEtapas.value.message)
+      : (message.value = etapasProcesso.value.message);
+    return (showMessage.value = true);
+  }
+});
+
+const onClickItem = (item) => {
+  etapaSelected.value = item;
+  dialog.value = true;
+};
+
+const onCreate = (etapa) => {
+  etapasProcesso.value.push(etapa.value);
+  dialog.value = false;
+};
+
+const onUpdate = (etapa) => {
+  const indexEtapa = etapasProcesso.value.findIndex(
+    (item) => item.id === etapa.value.id,
+  );
+  etapasProcesso.value[indexEtapa] = etapa.value;
+  dialog.value = false;
+};
+
+const formatarData = (data) => {
+  //utcToZonedTime(date, 'America/Sao_Paulo') -- Backend
+  const dataTimeZoned = utcToZonedTime(data, "-00:00"); //Ignora o timezone, mantendo a data correta
+  return format(dataTimeZoned, "dd/MM/yyyy");
+};
+</script>
