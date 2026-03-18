e257e0c743053ac85fbdef4055c9ee9e45507392
hermes.santos <hermes.santos@edu.itajai.sc.gov.br>
2026-03-18 18:51:39 -0300

    fix: fix 400 error

--- server/api/alunos/.put.js
+++ server/api/alunos/.put.js
@@ -1,6 +1,29 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const id = body.id;
+
+  if (body.sincronizacaoErudio) {
+    const alunoDTO = {
+      nome: body.nome,
+      cpf: body.cpf,
+      email: body.email,
+      dataNascimento: body.dataNascimento,
+      telefone1: body.telefone1,
+      telefone2: body.telefone2,
+      responsavelNome: body.responsavelNome,
+    };
+
+    const config = useRuntimeConfig();
+    const response = await fetch(`${config.public.baseURL}/alunos/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(alunoDTO),
+      headers: { "Content-type": "application/json" },
+    });
+
+    const data = await response.json();
+    return data;
+  }
+
   delete body.id;
   const config = useRuntimeConfig();
   
@@ -9,7 +32,7 @@ export default defineEventHandler(async (event) => {
     body: JSON.stringify(body),
     headers: { "Content-type": "application/json" },
   });
-  
+
   const data = await response.json();
   return data;
 });
